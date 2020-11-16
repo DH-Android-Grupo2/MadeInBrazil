ba4329083e9fabda94f852aa03e081e67182f9aa
Pedro Ivan Chaves Oliveira <pedroivanchaves@hotmail.com>
2020-11-16 14:29:41 -0300

    First Commit

---
+++ app/src/main/java/com/example/madeinbrasil/view/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.madeinbrasil.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.madeinbrasil.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
