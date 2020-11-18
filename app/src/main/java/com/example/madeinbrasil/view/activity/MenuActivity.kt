3b7f13a7d89e5045b27f009aabd3d147b3014328
Pedro Ivan Chaves Oliveira <pedroivanchaves@hotmail.com>
2020-11-18 18:29:33 -0300

    Fragments

--- app/src/main/java/com/example/madeinbrasil/view/activity/MenuActivity.kt
+++ app/src/main/java/com/example/madeinbrasil/view/activity/MenuActivity.kt
@@ -3,10 +3,30 @@ package com.example.madeinbrasil.view.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.madeinbrasil.R
+import com.example.madeinbrasil.view.fragment.FilmsFragment
+import com.example.madeinbrasil.view.fragment.SeriesFragment
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.button_home -> true
+                R.id.button_films -> {
+                    supportFragmentManager.beginTransaction().add(R.id.container, FilmsFragment()).commit()
+                    true
+                }
+                R.id.button_lists -> true
+                R.id.button_series -> {
+                    supportFragmentManager.beginTransaction().add(R.id.container, SeriesFragment()).commit()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
