39494c58fdc4416fcface180e4f27c5b0a101e66
Pedro Ivan Chaves Oliveira <pedroivanchaves@hotmail.com>
2020-11-18 16:56:15 -0300

    Recycler View e outros ícones

---
+++ app/src/main/java/com/example/madeinbrasil/view/fragment/FilmsFragment.kt
@@ -0,0 +1,24 @@
+package com.example.madeinbrasil.view.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.madeinbrasil.R
+
+class FilmsFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_films, container, false)
+    }
+
+}
