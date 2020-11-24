f1adf6d7d590d03aaa264c1ebf498639d5c9857e
Alexandre Araújo <a1lexandre.na@gmail.com>
2020-11-24 11:53:03 -0300

    Favorite Activity created

---
+++ app/src/main/java/com/example/madeinbrasil/AchievementActivity.kt
@@ -0,0 +1,15 @@
+package com.example.madeinbrasil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.madeinbrasil.databinding.ActivityAchievementBinding
+
+class AchievementActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAchievementBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAchievementBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
