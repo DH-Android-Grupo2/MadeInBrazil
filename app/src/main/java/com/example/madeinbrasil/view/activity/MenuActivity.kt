290ac30ab395478b1eb6b6c08e3218afb76200c6
Pedro Ivan Chaves Oliveira <pedroivanchaves@hotmail.com>
2020-11-24 14:23:40 -0300

    Layouts Profile Options

--- app/src/main/java/com/example/madeinbrasil/view/activity/MenuActivity.kt
+++ app/src/main/java/com/example/madeinbrasil/view/activity/MenuActivity.kt
@@ -2,11 +2,11 @@ package com.example.madeinbrasil.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.madeinbrasil.R
 import com.example.madeinbrasil.databinding.ActivityMenuBinding
 import com.example.madeinbrasil.view.fragment.FilmsFragment
 import com.example.madeinbrasil.view.fragment.SeriesFragment
-import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity() {
 
@@ -17,21 +17,27 @@ class MenuActivity : AppCompatActivity() {
         binding = ActivityMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        bottom_navigation.setOnNavigationItemSelectedListener {
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
             when(it.itemId) {
-                R.id.button_home -> false
-                R.id.button_films -> {
-                    supportFragmentManager.beginTransaction().add(R.id.container, FilmsFragment()).commit()
+                R.id.buttonHome -> false
+                R.id.buttonFilms -> {
+                    initFragments(FilmsFragment())
                     true
                 }
-                R.id.button_lists -> false
-                R.id.button_series -> {
-                    supportFragmentManager.beginTransaction().add(R.id.container, SeriesFragment()).commit()
+                R.id.buttonLists -> false
+                R.id.buttonSeries -> {
+                    initFragments(SeriesFragment())
                     true
                 }
                 else -> false
             }
         }
         supportActionBar?.hide()
+    }
+
+    private fun initFragments(fragment: Fragment) {
+        val fragmentStart = supportFragmentManager.beginTransaction()
+        fragmentStart.replace(R.id.flContainerMenu, fragment)
+        fragmentStart.commit()
     }
 }
