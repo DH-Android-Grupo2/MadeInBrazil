250b291435fd54921d19091cb1cf583dcbb02958
Pedro Ivan Chaves Oliveira <pedroivanchaves@hotmail.com>
2020-11-23 22:38:59 -0300

    Adicionando nome nas listas de filmes

--- app/src/main/java/com/example/madeinbrasil/view/adapter/ViewHolderFilm.kt
+++ app/src/main/java/com/example/madeinbrasil/view/adapter/ViewHolderFilm.kt
@@ -2,6 +2,7 @@ package com.example.madeinbrasil.view.adapter
 
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.madeinbrasil.R
@@ -11,8 +12,10 @@ import kotlinx.android.synthetic.main.main_cards.view.*
 class ViewHolderFilm (viewItem: View): RecyclerView.ViewHolder(viewItem) {
     fun bind(film: Films, position: (Int) -> Unit) = with(itemView) {
         val imageCardView = findViewById<ImageView>(R.id.cvImage)
+        val nameFilmCardView = findViewById<TextView>(R.id.filmNameRecyclerView)
 
         Glide.with(itemView.context).load(film.img).into(imageCardView)
+        nameFilmCardView.text = film.name
 
         vgMainCard.setOnClickListener {
             position(this@ViewHolderFilm.adapterPosition)
