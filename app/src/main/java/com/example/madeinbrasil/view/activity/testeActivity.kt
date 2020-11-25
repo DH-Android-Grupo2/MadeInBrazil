60d7d01fdf4dcc0411177c78622985d8af074467
Pedro Ivan Chaves Oliveira <pedroivanchaves@hotmail.com>
2020-11-25 10:53:20 -0300

    Feature Vincius

--- app/src/main/java/com/example/madeinbrasil/view/activity/testeActivity.kt
+++ app/src/main/java/com/example/madeinbrasil/view/activity/testeActivity.kt
@@ -2,6 +2,7 @@ package com.example.madeinbrasil.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.madeinbrasil.R
 
 class testeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
