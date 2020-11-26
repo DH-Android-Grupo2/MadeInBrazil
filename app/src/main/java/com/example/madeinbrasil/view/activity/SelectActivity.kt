3c1d9c265717a7ac61911337f57be8cb4820bc74
Pedro Ivan Chaves Oliveira <pedroivanchaves@hotmail.com>
2020-11-26 20:43:36 -0300

    Alteração nos chips

--- app/src/main/java/com/example/madeinbrasil/view/activity/SelectActivity.kt
+++ app/src/main/java/com/example/madeinbrasil/view/activity/SelectActivity.kt
@@ -3,10 +3,14 @@ package com.example.madeinbrasil.view.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.madeinbrasil.R
+import com.example.madeinbrasil.databinding.ActivitySelectBinding
 
 class SelectActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySelectBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_select)
+        binding = ActivitySelectBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
