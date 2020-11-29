81981a7185947605b04b570ef5e1c8203d8fb3fb
Pedro Ivan Chaves Oliveira <pedroivanchaves@hotmail.com>
2020-11-29 17:47:56 -0300

    Detalhes

--- app/src/main/java/com/example/madeinbrasil/model/search/movie/SearchMovie.kt
+++ app/src/main/java/com/example/madeinbrasil/model/search/movie/SearchMovie.kt
@@ -7,16 +7,10 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class SearchMovie(
-<<<<<<< HEAD
-        val page: Int,
-        var results: List<Result>,
-        @SerializedName("total_pages")
-=======
     val page: Int,
     var results: List<Result>,
     @SerializedName("total_pages")
->>>>>>> 43aedfac5808257d3f13b0692cca9510b202bd9d
     val totalPages: Int,
-        @SerializedName("total_results")
+    @SerializedName("total_results")
     val totalResults: Int
 ): Parcelable
