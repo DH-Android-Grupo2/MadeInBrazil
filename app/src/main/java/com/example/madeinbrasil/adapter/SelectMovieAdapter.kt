9b6de91d6e4035301181721c235990631b12ed2d
Alexandre Araújo <a1lexandre.na@gmail.com>
2020-12-09 09:51:40 -0300

    Fixing recyclerview's viewholder layout refresh issue

--- app/src/main/java/com/example/madeinbrasil/adapter/SelectMovieAdapter.kt
+++ app/src/main/java/com/example/madeinbrasil/adapter/SelectMovieAdapter.kt
@@ -9,10 +9,12 @@ import com.bumptech.glide.Glide
 import com.example.madeinbrasil.R
 import com.example.madeinbrasil.model.upcoming.Result
 import com.example.madeinbrasil.databinding.MainCardsSelectionBinding
+import kotlinx.android.synthetic.main.main_cards_selection.view.*
 
 class SelectMovieAdapter(): PagedListAdapter<Result, SelectMovieAdapter.ViewHolder>(Result.DIFF_CALLBACK) {
 
     var selectedItems = mutableListOf<Int>()
+    var onItemClicked: ((Int) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
@@ -22,19 +24,29 @@ class SelectMovieAdapter(): PagedListAdapter<Result, SelectMovieAdapter.ViewHold
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val movie = getItem(position)
+        Log.i("POSITION", position.toString())
         holder.bind(movie, selectedItems)
         holder.itemView.setOnClickListener {
             tooglePosition(movie)
             notifyItemChanged(position)
+            notifyDataSetChanged()
         }
+
+//        holder.itemView.setOnClickListener {
+//
+//        }
     }
 
     private fun tooglePosition(movie: Result?) {
         movie?.let {
-            if (selectedItems.contains(it.id))
+            if (selectedItems.contains(it.id)) {
                 selectedItems.remove(it.id)
-            else
+                Log.i("EXIS", "Estava selecionado")
+            }
+            else {
                 selectedItems.add(it.id)
+                Log.i("EXIS", "Não estava selecionado")
+            }
         }
     }
 
@@ -54,9 +66,12 @@ class SelectMovieAdapter(): PagedListAdapter<Result, SelectMovieAdapter.ViewHold
             Log.i("IDS", selectedItems.toString())
 
             movie?.let {
+                Log.i("ID", it.id.toString())
                 if(selectedItems.contains(it.id)) {
                     Log.i("ENTREI", "Mudei o id ${it.id}")
                     tvSelectionCover.setImageResource(R.drawable.remove_item)
+                } else {
+                    tvSelectionCover.setImageResource(R.drawable.add_item)
                 }
             }
 
