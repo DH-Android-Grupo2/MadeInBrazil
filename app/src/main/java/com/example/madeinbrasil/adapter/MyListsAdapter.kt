ba95841d53e211f8426a82fc151cdee3efb55833
Alexandre Araújo <a1lexandre.na@gmail.com>
2021-01-20 13:37:55 -0300

    list of lists showing covers

--- app/src/main/java/com/example/madeinbrasil/adapter/MyListsAdapter.kt
+++ app/src/main/java/com/example/madeinbrasil/adapter/MyListsAdapter.kt
@@ -1,8 +1,10 @@
 package com.example.madeinbrasil.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.RecyclerView
@@ -10,6 +12,7 @@ import com.bumptech.glide.Glide
 import com.example.madeinbrasil.R
 import com.example.madeinbrasil.databinding.MainMyListRecyclerviewBinding
 import com.example.madeinbrasil.model.customLists.ListWithMediaUni
+
 
 class MyListsAdapter(private val list: List<ListWithMediaUni>): RecyclerView.Adapter<MyListsAdapter.ViewHolder>() {
 
@@ -24,27 +27,50 @@ class MyListsAdapter(private val list: List<ListWithMediaUni>): RecyclerView.Ada
 
     override fun getItemCount(): Int = list.size
 
-    inner class ViewHolder(val binding: MainMyListRecyclerviewBinding): RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(val binding: MainMyListRecyclerviewBinding) : RecyclerView.ViewHolder(
+            binding.root
+    ) {
         fun bind(list: ListWithMediaUni) = with(binding) {
             val mediaSize = list.media.size
             if (mediaSize == 0) {
                 val textView = TextView(containerView.context)
                 textView.text = "Vazia"
-                textView.setTextColor(ResourcesCompat.getColor(containerView.context.resources, R.color.colorAccent, null))
+                textView.setTextColor(
+                        ResourcesCompat.getColor(
+                                containerView.context.resources,
+                                R.color.colorAccent,
+                                null
+                        )
+                )
                 containerView.addView(textView)
-            } else
-                if (mediaSize < 4) {
-                    for(i in 0 until mediaSize -1) {
+            } else if (mediaSize < 4) {
+                    for (i in 0 until mediaSize) {
                         val imageView = ImageView(containerView.context)
-                        imageView.maxHeight = 56
-                        imageView.maxWidth = 28
-                        Glide.with(containerView.context).load(list.media[i].backdropPath).into(imageView)
+//
+                        imageView.layoutParams = LinearLayout.LayoutParams(convertDp(102/mediaSize, containerView.context), LinearLayout.LayoutParams.MATCH_PARENT)
+                        Glide.with(containerView).load(list.media[i].backdropPath).into(imageView)
+                        imageView.scaleType = ImageView.ScaleType.CENTER_CROP
                         containerView.addView(imageView)
-                    }
-                }
+                        }
+                     } else {
+                        for (i in 0 until 4) {
+                            val imageView = ImageView(containerView.context)
+
+                            imageView.layoutParams = LinearLayout.LayoutParams(convertDp(26, containerView.context), LinearLayout.LayoutParams.MATCH_PARENT)
+                            Glide.with(containerView).load(list.media[i].backdropPath).into(imageView)
+                            imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+                            containerView.addView(imageView)
+                        }
+            }
 
             tvListName.text = list.list.name
         }
+
+        private fun convertDp(num: Int, context: Context): Int {
+            val scale: Float = context.resources.displayMetrics.density
+            return (num * scale + 0.5f).toInt()
+        }
+
     }
 
 }
