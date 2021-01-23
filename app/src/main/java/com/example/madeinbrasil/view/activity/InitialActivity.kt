1ebee8b0448cee41aafde85b0478a00e7b356e7d
Marcos Renann <marcos.renann.br@gmail.com>
2021-01-22 22:05:20 -0300

    Start Firebase

--- app/src/main/java/com/example/madeinbrasil/view/activity/InitialActivity.kt
+++ app/src/main/java/com/example/madeinbrasil/view/activity/InitialActivity.kt
@@ -5,24 +5,70 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import com.example.madeinbrasil.R
+import com.example.madeinbrasil.databinding.ActivityInitialBinding
+import com.firebase.ui.auth.AuthMethodPickerLayout
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.firebase.ui.auth.IdpResponse.fromResultIntent
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class InitialActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_initial)
+    private lateinit var binding: ActivityInitialBinding
 
-
-        findViewById<AppCompatButton>(R.id.btLoginInitial).setOnClickListener(openActivity(LogInActivity::class.java))
-        findViewById<AppCompatButton>(R.id.btRegisterInitial).setOnClickListener(openActivity(RegisterActivity::class.java))
+    private val providers by lazy {
+        arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
     }
 
-    private fun openActivity(activity: Class<out Activity>): View.OnClickListener?{
-        return View.OnClickListener {
-            startActivity(Intent(this, activity))
-        }
+    private val firebaseAuth by lazy {
+        Firebase.auth
+    }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityInitialBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val customLayout = AuthMethodPickerLayout.Builder(R.layout.activity_initial)
+            .setEmailButtonId(R.id.btLoginInitial)
+            .setGoogleButtonId(R.id.btGoogleInitial)
+            .build()
+
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setTheme(R.style.AppTheme)
+                .setAuthMethodPickerLayout(customLayout)
+                .setAvailableProviders(providers)
+                .setIsSmartLockEnabled(false)
+                .build(),
+            RC_SIGN_IN)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == Activity.RESULT_OK) {
+                val intent = Intent(this, SelectActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this,"Login não foi bem Sucedido",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    companion object {
+        private const val RC_SIGN_IN = 999
     }
 }
 
--- app/src/main/java/com/example/madeinbrasil/view/activity/SelectActivity.kt
+++ app/src/main/java/com/example/madeinbrasil/view/activity/SelectActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.core.view.get
 import androidx.recyclerview.widget.DiffUtil
 import com.example.madeinbrasil.R
@@ -16,17 +17,27 @@ import com.example.madeinbrasil.model.gender.GenreSelected
 import com.example.madeinbrasil.model.result.Genre
 import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipGroup
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_select.*
 import kotlinx.android.synthetic.main.activity_select.view.*
 import nl.bryanderidder.themedtogglebuttongroup.ThemedButton
 
 class SelectActivity : AppCompatActivity() {
     var selectedGenres = mutableListOf<String>()
+    var genres:String? = ""
 
+
+    private val db by lazy {
+        Firebase.firestore
+    }
+
+    private val firebaseAuth by lazy {
+        Firebase.auth
+    }
 
     private lateinit var binding: ActivitySelectBinding
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,11 +49,32 @@ class SelectActivity : AppCompatActivity() {
             selectedGenres.clear()
             selectedButtons.forEach {
                 selectedGenres.add("${it.tag} ,")
+                genres+="${it.tag} ,"
             }
-            Log.i("sdafa","${selectedGenres}")
+            registerDB()
             startMenuActivity(this@SelectActivity)
         }
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        firebaseAuth.currentUser?.let {
+            val documentReference = db.collection(FIREBASE_COLLECTION_USERS).document(it.uid)
+            documentReference.get()
+                    .addOnSuccessListener { snapshot ->
+
+                    }.addOnFailureListener {
+                        Toast.makeText(this, it.localizedMessage, Toast.LENGTH_SHORT).show()
+                    }
+        } ?: run {
+            signIn()
+        }
+    }
+
+    private fun signIn() {
+        startActivity(Intent(this, InitialActivity::class.java))
+        finish()
     }
 
     fun startMenuActivity(context: Context) {
@@ -50,6 +82,32 @@ class SelectActivity : AppCompatActivity() {
         intent.putExtra("genreList",GenreSelected(selectedGenres))
         startActivity(intent)
 
+    }
+
+
+    private fun registerDB() {
+        firebaseAuth.currentUser?.let { user->
+            val userData = hashMapOf(
+                    "name" to (user.displayName ?: ""),
+                    "email" to (user.email ?: ""),
+                    "phone" to (user.phoneNumber ?: ""),
+                    "genresSelected" to (genres ?: "")
+            )
+
+            db.collection(FIREBASE_COLLECTION_USERS)
+                    .document(user.uid ?: "")
+                    .set(userData)
+                    .addOnSuccessListener {
+                        Toast.makeText(this, "Dados salvos com sucesso", Toast.LENGTH_SHORT).show()
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(this, it.localizedMessage, Toast.LENGTH_SHORT).show()
+                    }
+        }
+    }
+
+    companion object {
+        private const val FIREBASE_COLLECTION_USERS = "users"
     }
 
 
