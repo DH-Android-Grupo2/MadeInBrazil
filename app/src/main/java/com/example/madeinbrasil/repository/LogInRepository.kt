7c8a264f8462166e4b05871f917c9ff40b8131ed
Marcos Renann <marcos.renann.br@gmail.com>
2021-02-11 12:11:41 -0300

    Solve some Login/Register Issues

--- app/src/main/java/com/example/madeinbrasil/repository/LogInRepository.kt
+++ app/src/main/java/com/example/madeinbrasil/repository/LogInRepository.kt
@@ -1,12 +1,10 @@
 package com.example.madeinbrasil.repository
 
+import android.util.Log
 import com.example.madeinbrasil.database.entities.User
 import com.example.madeinbrasil.utils.Constants
 import com.facebook.AccessToken
-import com.google.firebase.auth.FacebookAuthProvider
-import com.google.firebase.auth.FirebaseAuthException
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.*
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.ktx.firestore
@@ -23,8 +21,23 @@ class LogInRepository {
     }
 
     suspend fun signInAuthentication(email: String, password: String): FirebaseUser? {
-        firebaseAuth.signInWithEmailAndPassword(email, password).await()
-        return firebaseAuth.currentUser ?: throw FirebaseAuthException("", "")
+
+        return try{
+            firebaseAuth
+                .signInWithEmailAndPassword(email,password)
+                .await()
+            return firebaseAuth.currentUser
+        }catch (e : FirebaseAuthUserCollisionException){
+            Log.i("alams","${e}")
+            return null
+        }catch (e : FirebaseAuthInvalidCredentialsException){
+            Log.i("alams","${e}")
+            return null
+        }catch (e : Exception) {
+            Log.i("alams", "${e}")
+            return null
+        }
+
     }
 
     suspend fun logInWithGoogle(idToken: String?, user: User?): FirebaseUser? {
--- app/src/main/java/com/example/madeinbrasil/repository/RegisterRepository.kt
+++ app/src/main/java/com/example/madeinbrasil/repository/RegisterRepository.kt
@@ -1,14 +1,16 @@
 package com.example.madeinbrasil.repository
 
+import android.util.Log
+import android.widget.Toast
 import com.example.madeinbrasil.database.entities.User
 import com.example.madeinbrasil.utils.Constants.Firebase.DATABASE_USERS
-import com.google.firebase.auth.FirebaseAuthException
-import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.*
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.tasks.await
+
 
 class RegisterRepository {
     private val firebaseAuth by lazy {
@@ -20,8 +22,23 @@ class RegisterRepository {
     }
 
     suspend fun createNewUser(email: String, password: String, user: User): FirebaseUser? {
-        firebaseAuth.createUserWithEmailAndPassword(email, password).await()
-        db.set(user, SetOptions.merge()).await()
-        return firebaseAuth.currentUser ?: throw FirebaseAuthException("", "")
+
+        return try{
+            firebaseAuth
+                .createUserWithEmailAndPassword(email,password)
+                .await()
+            db.set(user, SetOptions.merge()).await()
+            return firebaseAuth.currentUser
+        }catch (e : FirebaseAuthUserCollisionException){
+            Log.i("alams","${e}")
+            return null
+        }catch (e : FirebaseAuthInvalidCredentialsException){
+            Log.i("alams","${e}")
+            return null
+        }catch (e : Exception) {
+            Log.i("alams", "${e}")
+            throw e
+        }
+
     }
 }
